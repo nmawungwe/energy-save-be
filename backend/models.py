74a63ab99c94f5266f0091613786d0f4bb2d89b0
Nyasha Vision <nmawungwe@gmail.com>
2020-09-20 12:10:34 +0200

    fixed whitenoise problem, could not load admin site

--- backend/models.py
+++ backend/models.py
@@ -4,3 +4,23 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 class User(AbstractUser):
     pass
+
+class Appliance(models.Model):
+    user = models.ForeignKey(User, related_name="appliances", on_delete=models.CASCADE)
+    TVs_num = models.IntegerField(blank = True)
+    Decoders_num = models.IntegerField(blank = True)
+    SoundSystems_num = models.IntegerField(blank = True)
+    Lights_num = models.IntegerField(blank = True)
+    Heaters_num = models.IntegerField(blank = True)
+    Stoves_num = models.IntegerField(blank = True)
+    Fridges_num = models.IntegerField(blank = True)
+    Kettles_num = models.IntegerField(blank = True)
+    Microwaves_num = models.IntegerField(blank = True)
+    Computers_num = models.IntegerField(blank = True)
+    Printers_num = models.IntegerField(blank = True)
+    Modems_num = models.IntegerField(blank = True)
+    ElectricBlanket_num = models.IntegerField(blank = True)
+    Phones_num = models.IntegerField(blank = True)
+
+
+
--- backend/views.py
+++ backend/views.py
@@ -4,8 +4,10 @@ from django.contrib.auth import authenticate, login, logout
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
-from .models import User
+from .models import User, Appliance
 
 # Create your views here.
 
@@ -54,3 +56,29 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "backend/register.html")
+
+
+@csrf_exempt
+def appliances(request):
+    if request.method == "GET":
+        pass
+    else:
+        TVs_num = request.POST["TVs_num"]
+        Decoders_num = request.POST["Decoders_num"]
+        SoundSystems_num = request.POST["SoundSystems_num"]
+        Lights_num = request.POST["Lights_num"]
+        Heaters_num = request.POST["Heaters_num"]
+        Stoves_num = request.POST["Stoves_num"]
+        Fridges_num = request.POST["Fridges_num"]
+        Kettles_num  = request.POST["Kettles_num"]
+        Microwaves_num  = request.POST["Microwaves_num"]
+        Computers_num  = request.POST["Computers_num"]
+        Printers_num  = request.POST["Printers_num"]
+        Modems_num  = request.POST["Modems_num"]
+        ElectricBlanket_num  = request.POST["ElectricBlanket_num"]
+        Phones_num  = request.POST["Phones_num"]
+        addition = Appliance(user=request.user.id, TVs_num=TVs_num, Decoders_num=Decoders_num, SoundSystems_num=SoundSystems_num, Lights_num=Lights_num , Heaters_num=Heaters_num, Stoves_num=Stoves_num, Fridges_num=Fridges_num, Kettles_num=Kettles_num, Microwaves_num=Microwaves_num, Computers_num=Computers_num, Printers_num=Printers_num, Modems_num=Modems_num, ElectricBlanket_num=ElectricBlanket_num, Phones_num=Phones_num)
+        addition.save()
+        return JsonResponse({"message":"Addition of appliances was successful"},)
+        
+
